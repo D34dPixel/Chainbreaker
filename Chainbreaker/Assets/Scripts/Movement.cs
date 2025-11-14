df142e69ea7aba70eb397da812a8b2a388a50ce8
D34dPixel <121385413+D34dPixel@users.noreply.github.com>
2025-11-14 11:51:23 +0000

    Added Gun Animations

--- Chainbreaker/Assets/Scripts/Movement.cs
+++ Chainbreaker/Assets/Scripts/Movement.cs
@@ -4,20 +4,33 @@ using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 public class Movement : MonoBehaviour
 {
+    public GameObject orient;
     [Header("Stats")]
     public int defaultSpeed = 20;
     public int sprintSpeed = 50;
-    public int slideForce = 25;
-    public float slideTime = 5;
     public int defaultHealth = 5;
     public int jumpForce = 10;
 
     int health;
     int speed;
-    float slideTimer;
     float jumpTimer;
 
-    [Header("Drag")]
+    [Header("Sliding")]
+    public int slideSpeed = 20;
+    public int slideForce = 25;
+    public float slideTime = 5;
+    float slideTimer;
+    float invcTimer;
+    public float invincibilityTime = 2;
+    Vector3 slideDir;
+
+    [Header("Ground")]
+    public GameObject Camera;
+    public Vector3 standardOffeset;
+
+
+    [Header("Ground")]
+    public float raycastDist;
     public LayerMask ground;
     public float groundDrag = 2f;
     public float airResistance = 10f;
@@ -25,14 +38,7 @@ public class Movement : MonoBehaviour
     Vector3 moveVector;
 
     //Action bools
-    bool moving;
-    bool sprinting;
-    bool grounded;
-    bool jumping;
-    bool falling;
-    bool shooting;
-    bool sliding;
-    bool facingRight;
+    bool moving,sprinting,grounded,jumping,falling,shooting,sliding,aiming,facingRight,invincible;
 
     Animator a;
     Rigidbody rb;
@@ -51,7 +57,7 @@ public class Movement : MonoBehaviour
     {
         a = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        //rb.freezeRotation = true;
         health = defaultHealth;
     }
     private void FixedUpdate()
@@ -65,6 +71,12 @@ public class Movement : MonoBehaviour
             }
         }
 
+        invcTimer -= Time.fixedDeltaTime;
+        if (invcTimer <= 0)
+        {
+            invincible = false;
+        }
+
         if (state == MoveState.inAir)
         {
             if (rb.linearVelocity.y <= 0)
@@ -74,7 +86,7 @@ public class Movement : MonoBehaviour
         }
 
         moving = (moveVector != Vector3.zero && state != MoveState.inAir && state != MoveState.sliding);
-        grounded = Physics.Raycast(transform.position+Vector3.up, Vector3.down, 1f, ground);
+        grounded = Physics.Raycast(transform.position+Vector3.up, Vector3.down, raycastDist, ground);
         StateHandler();
 
 
@@ -84,9 +96,12 @@ public class Movement : MonoBehaviour
         a.SetBool("Jumping", jumping);
         a.SetBool("Sliding", sliding);
         a.SetBool("Falling", falling);
+        a.SetBool("Aiming", aiming);
 
         if (state == MoveState.inAir)
             rb.AddForce(moveVector.normalized * speed*airResistance, ForceMode.Force);
+        else if (state == MoveState.sliding)
+            rb.AddForce(slideDir.normalized * speed, ForceMode.Force);
         else
             rb.AddForce(moveVector.normalized * speed, ForceMode.Force);
 
@@ -100,20 +115,22 @@ public class Movement : MonoBehaviour
     //basic movement input
     public void OnMove(InputValue moveVal)
     {
-        if (state != MoveState.sliding)
-        {
-            Vector2 moveVec = moveVal.Get<Vector2>();
-            moveVector = new Vector3(moveVec.x, 0, moveVec.y);
+        Vector2 moveVec = moveVal.Get<Vector2>();
+        moveVector = new Vector3(moveVec.x, 0, moveVec.y);
 
-            if (moveVector.x < 0) facingRight = true;
-            else if (moveVector.x > 0) facingRight = false;
-            GetComponentInChildren<SpriteRenderer>().flipX = facingRight;
-        }
+        if (moveVector.x < 0) facingRight = true;
+        else if (moveVector.x > 0) facingRight = false;
+        GetComponentInChildren<SpriteRenderer>().flipX = facingRight;
     }
 
     public void OnSprint()
     {
         sprinting = !sprinting;
+    }
+
+    public void OnAim()
+    {
+        aiming = !aiming;
     }
 
     public void OnJump()
@@ -133,11 +150,14 @@ public class Movement : MonoBehaviour
     {
         if (state != MoveState.inAir && state != MoveState.sliding)
         {
+            slideDir = moveVector;
             rb.mass = 0.5f;
             a.SetTrigger("Slide");
             sliding = true;
             slideTimer = slideTime;
+            invcTimer = invincibilityTime;
             if (moving) rb.AddForce(moveVector*slideForce, ForceMode.Impulse);
+            else rb.AddForce(orient.transform.forward * slideForce, ForceMode.Impulse);
         }
     }
 
@@ -153,6 +173,11 @@ public class Movement : MonoBehaviour
                 state = MoveState.walking;
                 speed = defaultSpeed;
 
+                if (aiming)
+                {
+                    speed /= 2;
+                }
+
                 if (sprinting)
                 {
                     state = MoveState.sprinting;
@@ -163,7 +188,7 @@ public class Movement : MonoBehaviour
             if (sliding)
             {
                 moving = false;
-                speed = 0;
+                speed = slideSpeed;
                 state = MoveState.sliding;
             }
         }
@@ -187,5 +212,11 @@ public class Movement : MonoBehaviour
         a.SetTrigger("End Jump");
         jumping = false;
         falling = true;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (!invincible)
+            health--;
     }
 }
